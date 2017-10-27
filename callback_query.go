0afd5c58af5ba28d4e18b2bb3ab777343c9adb90
cinos1 <cinos@qq.com>
2017-10-27 19:29:35 +0800

    add query redis cache and switch to redis

--- callback_query.go
+++ callback_query.go
@@ -15,6 +15,11 @@ func init() {
 	DefaultCallback.Query().Register("gorm:after_query", afterQueryCallback)
 }
 
+type SubObject struct {
+	isCache bool
+	Id      string
+}
+
 // queryCallback used to query data from database
 func queryCallback(scope *Scope) {
 	defer scope.trace(NowFunc())
@@ -56,16 +61,18 @@ func queryCallback(scope *Scope) {
 		if str, ok := scope.Get("gorm:query_option"); ok {
 			scope.SQL += addExtraSpaceIfExist(fmt.Sprint(str))
 		}
-
-		//get hasevalue from sql
+		iscache := reflect.ValueOf(scope.Value).Elem().FieldByName("isCache")
 		haskey := md5.Sum([]byte(scope.SQL))
 		haskeystr := fmt.Sprintf("%x", haskey) //将[]byte转成16进制
-		//is value exist?
-		if(Rds.HExists(scope.TableName(),haskeystr).Val()){
-			//get values from redis
-			redisValue, _ := Rds.HGet(scope.TableName(),haskeystr).Bytes()
-			json.Unmarshal(redisValue,scope.Value)
-			return
+		//iscache存在且其值为真，则调用redis缓存逻辑
+		if (iscache.IsValid() && iscache.Bool()) {
+			//is value exist?
+			if (Rds.HExists(scope.TableName(), haskeystr).Val()) {
+				//get values from redis
+				redisValue, _ := Rds.HGet(scope.TableName(), haskeystr).Bytes()
+				json.Unmarshal(redisValue, scope.Value)
+				return
+			}
 		}
 
 		if rows, err := scope.SQLDB().Query(scope.SQL, scope.SQLVars...); scope.Err(err) == nil {
@@ -90,13 +97,15 @@ func queryCallback(scope *Scope) {
 					}
 				}
 			}
-			//set redis value
-			jsonValue, _ := json.Marshal(scope.Value)
-			Rds.HSet(scope.TableName(),haskeystr,jsonValue)
-			if err := rows.Err(); err != nil {
-				scope.Err(err)
-			} else if scope.db.RowsAffected == 0 && !isSlice {
-				scope.Err(ErrRecordNotFound)
+			if (iscache.IsValid() && iscache.Bool()) {
+				//set redis value
+				jsonValue, _ := json.Marshal(scope.Value)
+				Rds.HSet(scope.TableName(), haskeystr, jsonValue)
+				if err := rows.Err(); err != nil {
+					scope.Err(err)
+				} else if scope.db.RowsAffected == 0 && !isSlice {
+					scope.Err(ErrRecordNotFound)
+				}
 			}
 		}
 	}
@@ -105,8 +114,6 @@ func queryCallback(scope *Scope) {
 // afterQueryCallback will invoke `AfterFind` method after querying
 func afterQueryCallback(scope *Scope) {
 	if !scope.HasError() {
-		//jsonValue, _ := json.Marshal(scope.Value)
-		//println(jsonValue)
 		scope.CallMethod("AfterFind")
 	}
 }
